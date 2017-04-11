3ba7f8316f1b7a60b9c944f0206456db15bb1062
hrfmmymt <hrfmmymt@gmail.com>
2017-04-11 10:52:59 +0900

    routing

---
+++ pages/about.js
@@ -0,0 +1,3 @@
+export default () => (
+  <p>Welcome to About!</p>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-import Head from 'next/head'
-
 import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Router from 'next/router'
+
+const handler = () => Router.push({
+  pathname: 'about',
+  query: { name: 'Zeit' }
+})
 
 export default class extends React.Component {
   static async getInitialProps ({ req }) {
@@ -14,15 +20,37 @@ export default class extends React.Component {
       <Head>
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <style>{`
+          body,
+          html,
+          div,
+          p,
+          img,
+          a,
+          ul,
+          li,
+          nav {
+            margin: 0;
+            padding: 0;
+          }
+        `}
+        </style>
       </Head>
       <p>Hello World {this.props.userAgent}</p>
       <img src="/static/hrfmmymt.jpg" />
+      <nav>
+        <ul>
+          <li><Link prefetch href='/'><a>Home</a></Link></li>
+          <li><Link prefetch href='/about'><a>About</a></Link></li>
+          <li><Link prefetch href='/contact'><a>Contact</a></Link></li>
+        </ul>
+      </nav>
       <style jsx>{`
         p {
           color: #999;
         }
         div {
-          background: red;
+          background: #f0f0f0;
         }
         img {
           width: 20%;
